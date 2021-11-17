d4e8e92a956650447e19e8b619aad878b401f8e7
sanket8498 <walke_sanket.ee@ghrce.raisoni.net>
2021-11-17 19:48:38 +0530

    first commit

---
+++ area_sqr.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int b,l;
+	printf("Enter the value of breath : ");
+	scanf("%d",&b);
+	printf("Enter the value of length : ");
+	scanf("%d",&l);
+	printf("The area of the rectangle is %d",l*b);
+	return 0;
+}
+
